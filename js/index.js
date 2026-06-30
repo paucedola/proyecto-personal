f913819430a88daed4c3c3ca7f4c56a6a336e4dd
paucedola <paucedola@gmail.com>
2026-06-30 14:44:03 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,5 +2,13 @@ document.addEventListener ("DOMcontenLoaded", () => {
   console.log("Primedic Salud");
 const titulo = document.querySelector("h1");
 const secction = document.querySelectorAll("section");
+ const links = document.querySelectorAll(".card-link");
+
+  links.forEach(link => {
+    link.addEventListener("click", () => {
+      console.log("Navegando a:", link.href);
+    });
+  });
+});
   
   
