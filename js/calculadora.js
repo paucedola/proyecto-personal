696d6035aa7b27f6dea653950aaf9acb23c74356
paucedola <paucedola@gmail.com>
2026-06-30 10:28:30 -0300

    Check if file is selected before reading
    cambios

--- js/calculadora.js
+++ js/calculadora.js
@@ -6,6 +6,8 @@ window.addEventListener("DOMContentLoaded", () => {
   inputFile.addEventListener("change", (e) => {
     const file = e.target.files[0];
 
+    if (!file) return;
+
     const reader = new FileReader();
 
     reader.onload = (event) => {
