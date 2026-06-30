df7c673b130eac1559aec2187ba00121ca9d151f
paucedola <paucedola@gmail.com>
2026-06-30 13:51:37 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-
+document.addEventListener ("DOMcontenLoaded", () => {
+  console.log("Primedic Salud");
+const titulo = document.querySelector("h1");
+const secction = document.querySelectorAll("section");
+  
+  
